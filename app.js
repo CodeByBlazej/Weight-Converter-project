fd606b68fd870bc13b943c60bd2f8a2ea147c740
CodeByBlazej <codebyblazej@gmail.com>
2024-01-07 19:40:48 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const inputEl = document.getElementById('input');
 const errorEl = document.getElementById('error');
 const resultEl = document.getElementById('result');
+
+
 let errorTime;
 let positiveTime;
 
